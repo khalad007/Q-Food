1154d1576983951e6866a0082fd730c42387ec68
khalad007 <www.arifhossain49919@gmail.com>
2023-11-05 01:49:55 -0800

    Added Error Page

---
+++ src/Pages/Error/Error.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+
+const Error = () => {
+    return (
+        <div>
+            <div className="flex justify-center">
+                <img className="h-[80vh]" src="https://i.ibb.co/bd3CCwm/Pngtree-socket-404-error-page-for-5228459.png" alt="Error" />
+            </div>
+            <div className="flex justify-center">
+                <Link to="/"><button className="btn bg-[#247CC6] text-white">Go Back To Home</button></Link>
+            </div>
+        </div>
+    );
+};
+
+export default Error;
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -4,11 +4,13 @@ import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
 import AddFood from "../Pages/AddFood/AddFood";
+import Error from "../Pages/Error/Error";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      errorElement: <Error></Error>,
       children: [
         {
             path: '/',
